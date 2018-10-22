eace0a9a9b6cfb363c15ef802d91f3c0197a0f37
Raj-18110 <sunandan.raj18110@gmail.com>
2018-10-22 10:23:56 +0530

    Add files via upload

---
+++ JPAHibernateOneToManyMapping/src/com/cg/project/beans/Customer.java
@@ -0,0 +1,19 @@
+package com.cg.project.beans;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+@Entity
+public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private int customerNo;
+	private String firstName,lastName,email;
+	@OneToMany(mappedBy="customer")
+	private List<Car>car;
+	public Customer() {
+		super();
+	}
+}
